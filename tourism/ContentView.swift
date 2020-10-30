daf17da5a9e865ffaa4f1c145fe912e7e8cc3b3b
Ihwan <mynameisihwan@gmail.com>
2020-10-30 15:07:09 +0700

    Initial Commit

---
+++ tourism/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  tourism
+//
+//  Created by Ihwan ID on 30/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ tourism/tourismApp.swift
@@ -0,0 +1,17 @@
+//
+//  tourismApp.swift
+//  tourism
+//
+//  Created by Ihwan ID on 30/10/20.
+//
+
+import SwiftUI
+
+@main
+struct tourismApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
